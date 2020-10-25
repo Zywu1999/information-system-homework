7bdc736fc062b7edfa284b6742e76a917e16298b
yuzuo <yuzuo4null@gmail.com>
2020-10-25 16:12:54 +0800

    尝试一下bootstrap的例子

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router=express.Router();
 
 //登录界面，并接受登录界面的POST请求,render的第一个参数是views文件夹下的文件
 router.get('/',function(req,res,next){
-    res.render('login',{title:'登录'});
+    res.render('bootstrapLogin',{title:'登录'});
 });
 router.post('/',function(req,res,next){
     res.render('index',{title:'首页'});
